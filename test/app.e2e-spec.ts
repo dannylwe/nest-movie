3409e04054b38acaf550473f4a2fc81a832938fc
unknown <dannylwe11@gmail.com>
2021-02-07 15:27:47 +0300

    GET and POST movie e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -20,5 +20,29 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  describe('/movies', ()=> {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('POST', () => {
+      let fakeMovie = { title: 'test', year: 2020, genre: ['action'] };
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send(fakeMovie)
+        .expect(201)
+        .expect([ { id: 1, title: 'test', year: 2020, genre: [ 'action' ] } ]);
+    });
+  });
+
+  describe('/movies/:id', ()=> {
+    it.todo('GET');
+    it.todo('PATCH');
+    it.todo('DELETE');
   });
 });
